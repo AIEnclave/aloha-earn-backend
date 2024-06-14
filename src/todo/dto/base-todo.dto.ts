f4c31c109d463c6558d300ebd04eceee121e49f9
ashwinsingh2007 <legend.ashwini07@gmail.com>
2024-06-14 23:19:40 +0530

    added class validator, class transformer, http status error handling

--- src/todo/dto/base-todo.dto.ts
+++ src/todo/dto/base-todo.dto.ts
@@ -1,4 +1,18 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+import { Expose, Transform } from 'class-transformer';
+
 export class BaseTodoDto {
-    title: string
+    @IsString()
+    @IsNotEmpty()
+    @Expose()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Expose()
     description?: string;
+
+    @Expose()
+    @Transform(({ value }) => new Date(value), { toClassOnly: true })
+    createdAt: Date;
 }
